f707f1bf48ad3c26ac832a00984fe8a7c985723b
Onur Dabanca <onurdbnc@gmail.com>
2024-06-05 18:12:55 +0300

    Add project files.

---
+++ Services/Catalog/MultiShopProject.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShopProject.Catalog.Entities;
+
+public class ProductImages
+{
+    public string ProductImagesID { get; set; }
+    public string ImgOne { get; set; }
+    public string ImgSecond { get; set; }
+    public string ImgThird { get; set; }
+    public string ProductId { get; set; }
+    public Product Product { get; set; }
+}
