26a7f5b93cd1b7b623f5de2fa321556cd5605dfe
Onur Dabanca <onurdbnc@gmail.com>
2024-06-09 19:03:08 +0300

    MultiShopProject.Discount project created. db connections, entities, services created.

---
+++ Services/Discount/MultiShopProject.Discount/Context/DapperContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShopProject.Discount.Entities;
+using System.Data;
+
+namespace MultiShopProject.Discount.Context;
+
+public class DapperContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+    public DapperContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=MultiShopDiscountDb; Trusted_Connection=True;");
+    }
+    public DbSet<Coupon> Coupons { get; set; }
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+}
---
+++ Services/Discount/MultiShopProject.Discount/Services/IDiscountService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShopProject.Discount.Dtos;
+
+namespace MultiShopProject.Discount.Services;
+
+public interface IDiscountService
+{
+    Task<List<ResultCouponDto>> GetAllCouponAsync();
+    Task CreateCouponAsync(CreateCouponDto createCouponDto);
+    Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
+    Task DeleteCouponAsync(int id);
+    Task<GetByIdCouponDto> GetByIdCouponAsync(int id);
+}
