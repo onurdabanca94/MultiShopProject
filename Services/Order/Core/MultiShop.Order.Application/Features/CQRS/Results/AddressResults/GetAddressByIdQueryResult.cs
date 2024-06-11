3a5df433e844af3b3e9aa1db8206cca54c959d5a
Onur Dabanca <onurdbnc@gmail.com>
2024-06-11 18:42:04 +0300

    Order Service created via CQRS Onion architecture.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/AddressResults/GetAddressByIdQueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+
+public class GetAddressByIdQueryResult
+{
+    public int AddressId { get; set; }
+    public string UserId { get; set; }
+    public string Disctrict { get; set; }
+    public string City { get; set; }
+    public string Detail { get; set; }
+}
---
+++ Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Order.Domain.Entities;
+
+public class Address
+{
+    public int AddressId { get; set; }
+    public string UserId { get; set; }
+    public string Disctrict { get; set; }
+    public string City { get; set; }
+    public string Detail { get; set; }
+}
