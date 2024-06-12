a8b51b309b5cae61d8663a9a6d8c765b2bb153be
Onur Dabanca <onurdbnc@gmail.com>
2024-06-12 17:59:54 +0300

    Commands and Handlers createad for Address.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+
+public class RemoveAddressCommand
+{
+    public int Id { get; set; }
+    public RemoveAddressCommand(int id)
+    {
+        Id = id;
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/UpdateAddressCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+
+public class UpdateAddressCommand
+{
+    public int AddressId { get; set; }
+    public string UserId { get; set; }
+    public string Disctrict { get; set; }
+    public string City { get; set; }
+    public string Detail { get; set; }
+}
