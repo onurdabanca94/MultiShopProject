51115b77772eb7a050ca0f8fb5837014f76361df
Onur Dabanca <onurdbnc@gmail.com>
2024-06-24 17:30:41 +0300

    Cargo layer created. Migrations created.

---
+++ Services/Cargo/MultiShopProject.Cargo.DataAccess/Concrete/CargoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShopProject.Cargo.Entity.Concrete;
+
+namespace MultiShopProject.Cargo.DataAccess.Concrete;
+
+public class CargoContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1441\\MSSQLLocalDB;Database=MultiShopCargoDb;User=sa;Password=123456aA*;TrustServerCertificate=True;MultipleActiveResultSets=True");
+    }
+    public DbSet<CargoCompany> CargoCompanies { get; set; }
+    public DbSet<CargoDetail> CargoDetails { get; set; }
+    public DbSet<CargoCustomer> CargoCustomers { get; set; }
+    public DbSet<CargoOperation> CargoOperations { get; set; }
+}
---
+++ Services/Cargo/MultiShopProject.Cargo.Entity/Concrete/CargoCompany.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShopProject.Cargo.Entity.Concrete;
+
+public class CargoCompany
+{
+    public int CargoCompanyId { get; set; }
+    public string CargoCompanyName { get; set; }
+}
