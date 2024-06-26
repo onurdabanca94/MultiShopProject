b65349c00bfddd35c93bf2d869daefe62c699973
Onur Dabanca <onurdbnc@gmail.com>
2024-06-26 18:18:45 +0300

    Navbar, Topbar, Carousel components created new images inserted.

---
+++ Frontends/MultiShopProject.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopProject.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _CarouselDefaultComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ Frontends/MultiShopProject.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopProject.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _SpecialOfferComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
