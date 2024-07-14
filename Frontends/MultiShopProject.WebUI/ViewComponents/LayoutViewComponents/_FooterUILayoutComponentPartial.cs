6d42dd7ae1de126b9645df17d8231abffbffd8b5
Onur Dabanca <onurdbnc@gmail.com>
2024-07-14 04:19:22 +0300

    Footer About renewed.

--- Frontends/MultiShopProject.WebUI/ViewComponents/LayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/MultiShopProject.WebUI/ViewComponents/LayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShopProject.Dto.CatalogDtos.AboutDtos;
+using Newtonsoft.Json;
 
 namespace MultiShopProject.WebUI.ViewComponents.LayoutViewComponents;
 
 public class _FooterUILayoutComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7192/api/Abouts");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
