640e5932cde1eeec7101073554e37b3e0f6ebc7d
Onur Dabanca <onurdbnc@gmail.com>
2024-07-21 18:55:24 +0300

    Register Form renewed. CreateRegisterDto created. _ViewImports referenced. RegisterController created.

--- Frontends/MultiShopProject.WebUI/Controllers/RegisterController.cs
+++ Frontends/MultiShopProject.WebUI/Controllers/RegisterController.cs
@@ -1,13 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShopProject.Dto.IdentityDtos.RegisterDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShopProject.WebUI.Controllers
 {
-    public class RegisterController : Controller
-    {
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class RegisterController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public RegisterController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateRegisterDto createRegisterDto)
+		{
+			if (createRegisterDto.Password == createRegisterDto.ConfirmPassword)
+			{
+				var client = _httpClientFactory.CreateClient();
+				var jsonData = JsonConvert.SerializeObject(createRegisterDto);
+				StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); //Content olarak atanan, bu contentin hangi dil desteği, mediatorün ne olduğu.
+				var responseMessage = await client.PostAsync("http://localhost:5001/api/Register", stringContent);
+				if (responseMessage.IsSuccessStatusCode)
+				{
+					return RedirectToAction("Index", "Login");
+				}
+			}
+			return View();
+		}
+	}
 }
