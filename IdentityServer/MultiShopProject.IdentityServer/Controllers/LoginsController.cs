a3b6daf3aeb6abd5e2620a70e7db5878c3ee3e97
Onur Dabanca <onurdbnc@gmail.com>
2024-07-22 17:49:12 +0300

    User login created. User login controller created. User login dto created. User login information added.

---
+++ IdentityServer/MultiShopProject.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShopProject.IdentityServer.Dtos;
+using MultiShopProject.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShopProject.IdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginsController : ControllerBase
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public LoginsController(SignInManager<ApplicationUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
+		{
+			var result = await _signInManager.PasswordSignInAsync(userLoginDto.Username, userLoginDto.Password, false, false);
+			if (result.Succeeded)
+			{
+				return Ok("Login has been successful!");
+			}
+			else
+			{
+				return Ok("Username or Password was wrong!");
+			}
+		}
+	}
+}
---
+++ IdentityServer/MultiShopProject.IdentityServer/Dtos/UserLoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShopProject.IdentityServer.Dtos;
+
+public class UserLoginDto
+{
+	public string Username { get; set; }
+	public string Password { get; set; }
+}
