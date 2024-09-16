8ca2350f9d5bbecf50684bd1f5f9f5075ef398b1
Onur Dabanca <onurdbnc@gmail.com>
2024-09-16 18:14:30 +0300

    Payment Credit Card page created.

---
+++ Frontends/MultiShopProject.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopProject.WebUI.Controllers;
+
+public class PaymentController : Controller
+{
+    public IActionResult Index()
+    {
+        DirectoryList();
+        return View();
+    }
+    public void DirectoryList()
+    {
+        ViewBag.directoryProject = "MultiShop Project";
+        ViewBag.directoryMain = "Ödeme Ekranı";
+        ViewBag.directoryProduct = "Kartla Ödeme";
+    }
+}
