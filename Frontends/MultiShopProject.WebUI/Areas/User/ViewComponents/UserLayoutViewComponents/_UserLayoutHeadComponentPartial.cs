eba57592353dcbdb9746f3017d3aeae5be3214fd
Onur Dabanca <onurdbnc@gmail.com>
2024-09-30 15:43:20 +0300

    UserLayout page components seperated to different pages. View pages created.

---
+++ Frontends/MultiShopProject.WebUI/Areas/User/ViewComponents/UserLayoutViewComponents/_UserLayoutHeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopProject.WebUI.Areas.User.ViewComponents.UserLayoutViewComponents;
+
+public class _UserLayoutHeadComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ Frontends/MultiShopProject.WebUI/Areas/User/ViewComponents/UserLayoutViewComponents/_UserLayoutSideBarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopProject.WebUI.Areas.User.ViewComponents.UserLayoutViewComponents;
+
+public class _UserLayoutSideBarComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
