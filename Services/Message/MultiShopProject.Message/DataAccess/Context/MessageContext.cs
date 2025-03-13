b6db59879c7d81eb8ec3eecbf3f693c98d71d2eb
Onur Dabanca <onurdbnc@gmail.com>
2025-03-13 14:32:19 +0300

    Message microservice created with PostgreSQL. Migration created. Message model created. Configuration added.

---
+++ Services/Message/MultiShopProject.Message/DataAccess/Context/MessageContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShopProject.Message.DataAccess.Entities;
+
+namespace MultiShopProject.Message.DataAccess.Context
+{
+    public class MessageContext : DbContext
+    {
+        public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+        {
+
+        }
+        public DbSet<UserMessage> UserMessages { get; set; }
+    }
+}
