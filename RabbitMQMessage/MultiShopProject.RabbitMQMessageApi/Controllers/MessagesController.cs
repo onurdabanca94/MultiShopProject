24035340080f191493aa0c6a1876b79e009c5e78
Onur Dabanca <onurdbnc@gmail.com>
2025-04-07 19:05:33 +0300

    RabbitMQ HttpGet ReadMessage endpoint inserted.

--- RabbitMQMessage/MultiShopProject.RabbitMQMessageApi/Controllers/MessagesController.cs
+++ RabbitMQMessage/MultiShopProject.RabbitMQMessageApi/Controllers/MessagesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace MultiShopProject.RabbitMQMessageApi.Controllers;
@@ -9,7 +10,7 @@ namespace MultiShopProject.RabbitMQMessageApi.Controllers;
 public class MessagesController : ControllerBase
 {
     [HttpPost]
-    public async Task<IActionResult> CreateMessage()
+    public IActionResult CreateMessage()
     {
         var connectionFactory = new ConnectionFactory
         {
@@ -20,15 +21,48 @@ public class MessagesController : ControllerBase
 
         var channel = connection.CreateModel();
 
-        channel.QueueDeclare("Kuyruk1", false, false, false, arguments: null);
+        channel.QueueDeclare("Kuyruk2", false, false, false, arguments: null);
 
-        var message = "Merhaba bu bir RabbitMQ kuyruk mesajıdır.";
+        var message = "Merhaba bugün o gün başaracağım!";
 
         var byteMessageContent = Encoding.UTF8.GetBytes(message);
 
 
-        channel.BasicPublish(exchange: "", routingKey: "Kuyruk1", basicProperties: null, body: byteMessageContent);
+        channel.BasicPublish(exchange: "", routingKey: "Kuyruk2", basicProperties: null, body: byteMessageContent);
 
         return Ok("Mesajınız kuyruğa alınmıştır.");
+    }
+
+    private static string message;
+
+    [HttpGet]
+    public IActionResult ReadMessage()
+    {
+        var factory = new ConnectionFactory();
+
+        factory.HostName = "localhost";
+
+        var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, x) =>
+        {
+            var byteMessage = x.Body.ToArray();
+            message = Encoding.UTF8.GetString(byteMessage);
+        };
+
+        channel.BasicConsume(queue: "Kuyruk2", autoAck: false, consumer: consumer);
+
+        if (string.IsNullOrEmpty(message))
+        {
+            return NoContent();
+        }
+        else
+        {
+            return Ok(message);
+        }
     }
 }
