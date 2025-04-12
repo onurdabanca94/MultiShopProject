7715909b7399c328ed5bac6cf6444823b6d54901
Onur Dabanca <onurdbnc@gmail.com>
2025-04-12 03:09:00 +0300

    Mail sending process & endpoint created.

---
+++ Frontends/MultiShopProject.WebUI/Controllers/MailController.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MultiShopProject.WebUI.Models;
+
+namespace MultiShopProject.WebUI.Controllers;
+
+public class MailController : Controller
+{
+    [HttpGet]
+    public IActionResult SendMail()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SendMail(MailRequest mailRequest)
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+
+        MailboxAddress mailboxAddressFrom = new MailboxAddress("MultiShopProject Katalog", "abc@gmail.com");
+        mimeMessage.From.Add(mailboxAddressFrom);
+
+        MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+        mimeMessage.To.Add(mailboxAddressTo);
+
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.TextBody = mailRequest.MessageContent;
+        mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+        mimeMessage.Subject = mailRequest.Subject;
+
+        SmtpClient client = new SmtpClient();
+        client.Connect("smtp.gmail.com", 587, false);
+        client.Authenticate("abc@gmail.com", "google ikili doğrulama şifresi");
+        client.Send(mimeMessage);
+        client.Disconnect(true);
+
+        return View();
+    }
+}
---
+++ Frontends/MultiShopProject.WebUI/Models/MailRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShopProject.WebUI.Models;
+
+public class MailRequest
+{
+    public string ReceiverMail { get; set; }
+    public string Subject { get; set; }
+    public string MessageContent { get; set; }
+}
